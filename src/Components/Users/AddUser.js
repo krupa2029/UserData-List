625a2379a325e988a3a5489821356f24c08b208d
krupa2029 <kjkotia10@gmail.com>
2021-10-01 19:23:42 +0530

    Added: AddUser Form with styling Compo: AddUser, Card

---
+++ src/Components/Users/AddUser.js
@@ -0,0 +1,25 @@
+import react from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
+const AddUser = props => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Card cssClass={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">UserName</label>
+                <input id="username" type="text"></input>
+
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number"></input>
+
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
+    );
+};
+
+export default AddUser;
