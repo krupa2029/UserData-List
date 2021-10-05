c3a512d0fdd1f1c44b876dd0d6c19466ad6c03b7
krupa2029 <kjkotia10@gmail.com>
2021-10-05 23:55:40 +0530

    Used: Portal for ErrorModal, Added Fragment

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -3,12 +3,13 @@ import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import classes from './AddUser.module.css';
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = props => {
 
     const [enteredUserName, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
-    const [error, setError]=useState();
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
@@ -44,21 +45,27 @@ const AddUser = props => {
     };
 
     return (
-        <div>
-        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
-        <Card className={classes.input}>
+        <Wrapper>
+            {error && (
+                <ErrorModal
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
+                />
+            )}
+            <Card className={classes.input}>
 
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">UserName</label>
-                <input id="username" type="text" value={enteredUserName} onChange={usernameChangeHandler}></input>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">UserName</label>
+                    <input id="username" type="text" value={enteredUserName} onChange={usernameChangeHandler}></input>
 
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
 
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
-        </div>
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </Wrapper>
     );
 };
 
